7f919b9cf19d906edd3a9b82754ca34f403b139a
shintaro <217023@himejo.jp>
2023-10-29 21:59:20 +0900

    1

--- app/src/main/java/com/example/games/GomokuActivity.java
+++ app/src/main/java/com/example/games/GomokuActivity.java
@@ -2,16 +2,31 @@ package com.example.games;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.games.gomokuData.GoStoneCoordinateData;
+
 public class GomokuActivity extends AppCompatActivity {
+    private TextView coordinate;
+    GoStoneCoordinateData goStoneCoordinateData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gomoku);
+        coordinate = findViewById(R.id.coordinate);
+        goStoneCoordinateData =new GoStoneCoordinateData();
         findViewById(R.id.home).setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class));
         });
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        //TextViewに表示
+        coordinate.setText("座標X:"+String.valueOf(event.getX())+" 座標Y:"+String.valueOf(event.getY()));
+        goStoneCoordinateData.printCoordinate();
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/games/gomokuData/GoStoneCoordinateData.java
@@ -0,0 +1,50 @@
+package com.example.games.gomokuData;
+
+import android.util.Log;
+
+public class GoStoneCoordinateData {
+    final String TAG = "GoStoneCoordinateData";
+    final int SQUARE_WIDTH = 58; //マスの正方形の幅
+    Coordinate[][] coordinates = new Coordinate[18][18];
+
+    public GoStoneCoordinateData() {
+        coodinateInit();
+        coordinates[0][0].X = 20;
+        coordinates[0][0].Y = 588;
+        for (int i = 0; i < coordinates.length; i++) {
+            for (int j = 0; j < coordinates[i].length; j++) {
+                if (i == 0 && j == 0) continue;
+                if (j != 0) coordinates[i][j].X = coordinates[i][j - 1].X;
+                if (i != 0) coordinates[i][j].Y = coordinates[i - 1][j].Y;
+                if (j == 0) coordinates[i][j].X = coordinates[i - 1][j].X;
+                if (i == 0) coordinates[i][j].Y = coordinates[i][j - 1].Y;
+            }
+        }
+    }
+
+    public class Coordinate {
+        public int X;
+        public int Y;
+    }
+
+    void coodinateInit() {
+        for (int i = 0; i < coordinates.length; i++) {
+            for (int j = 0; j < coordinates[i].length; j++) {
+                coordinates[i][j] = new Coordinate();
+            }
+        }
+    }
+
+    public void printCoordinate() {
+        for (int i = 0; i < coordinates.length; i++) {
+            for (int j = 0; j < coordinates[i].length; j++) {
+                debug("X:" + coordinates[i][j].X + " Y:" + coordinates[i][j].Y);
+            }
+        }
+    }
+
+    public void debug(String msg) {
+        Log.d(TAG, msg);
+    }
+}
+
